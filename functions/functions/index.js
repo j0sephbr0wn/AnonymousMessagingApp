6b69a85610a82b48e5524255eea6d6a5f487fc60
j0sephbr0wn <43600213+j0sephbr0wn@users.noreply.github.com>
2020-05-24 15:57:00 +0100

    Cloud Functions setup

---
+++ functions/functions/index.js
@@ -0,0 +1,38 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// function to notify anonymous users when a champion comes online
+exports.sendAdminNotification = functions.database.ref('/Users/{pushId}')
+    .onUpdate((change, context) => {
+      // check if status was offline previously
+      const beforeUser = change.before.val();
+      const beforeStatus = beforeUser.status;
+      if (beforeStatus==="offline") {
+        // check if status is online now
+        const afterUser = change.after.val();
+        const afterStatus = afterUser.status;
+        if (afterStatus==="online") {
+          // build notification
+          const payload = {
+            notification: {
+              title: 'Champion online!',
+              body: `${afterUser.username}`
+            }
+          };
+
+          // send nofifications to topic subscribers
+          return admin.messaging()
+            .sendToTopic("anonymousUsers",payload)
+            .then(response => {
+              return console.log('Notification sent successfully', response);
+            })
+            .catch(error => {
+              console.log('Notification sent failed:',error);
+            });
+        }
+      }
+
+      return console.log('No change from Offline to Online, notification not sent');
+});
+
